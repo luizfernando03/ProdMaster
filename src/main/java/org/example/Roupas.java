cee76ef912f2d09a8a59b6b345eb646648968ae0
Luiz Fernando <luizfernandodemoura.2013@gmail.com>
2024-06-10 22:42:52 -0300

    Criando a classe Roupas

---
+++ src/main/java/org/example/Roupas.java
@@ -0,0 +1,43 @@
+package org.example;
+
+public class Roupas extends ProdutoAbstrato {
+
+    private String tamanho;
+    private String cor;
+
+    public Roupas(String nome, double preco, String tamanho, String cor, int quantidade) {
+        super(nome, preco, quantidade);
+        this.tamanho = tamanho;
+        this.cor = cor;
+    }
+
+    public Roupas() {}
+
+    public String getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    @Override
+    public String getTipo() {
+        return "Roupas";
+    }
+
+    @Override
+    public String exibirDetalhes() {
+        return "Tipo: " + getTipo() + ", Nome: " + getNome() + ", Preço: " + getPreco() +
+                ", Quantidade: " + getQuantidade() + ", Tamanho: " + tamanho + ", Cor: " + cor;
+    }
+
+}
