d5ef5e9be1ddedcaf3fcf86ec5b94916873b1c01
Luiz Fernando <luizfernandodemoura.2013@gmail.com>
2024-06-10 22:39:56 -0300

    Criando a classe Eletronicos

---
+++ src/main/java/org/example/Eletronicos.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public abstract class Eletronicos extends ProdutoAbstrato{
+    private int garantiaMeses;
+
+    public Eletronicos(String nome, double preco, int garantiaMeses, int quantidade) {
+        super(nome, preco, quantidade);
+        this.garantiaMeses = garantiaMeses;
+    }
+
+    public Eletronicos() {}
+
+    public int getGarantiaMeses() {
+        return garantiaMeses;
+    }
+
+    public void setGarantiaMeses(int garantiaMeses) {
+        this.garantiaMeses = garantiaMeses;
+    }
+
+    @Override
+    public String getTipo() {
+        return "Eletrônico";
+    }
+
+    @Override
+    public String exibirDetalhes() {
+        return "Tipo: " + getTipo() + ", Nome: " + getNome() + ", Preço: " + getPreco() +
+                ", Quantidade: " + getQuantidade() + ", Garantia: " + garantiaMeses + " meses";
+    }
+
+}
