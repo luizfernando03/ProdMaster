ee4eb6f56e91a58b5d1aa9d8d9406bea3534b7ed
Luiz Fernando <luizfernandodemoura.2013@gmail.com>
2024-06-10 22:15:20 -0300

    Crinado interface Produto

---
+++ src/main/java/org/example/Produto.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public interface Produto {
+        String getTipo();
+        String getNome();
+        double getPreco();
+        void setNome(String nome);
+        void setPreco(double preco);
+        void setQuantidade(int quantidade);
+        int getQuantidade();
+        String exibirDetalhes();
+
+}
