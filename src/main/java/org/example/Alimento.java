79261b9a2d63febbdf9141a37c3d68373b34ab17
Luiz Fernando <luizfernandodemoura.2013@gmail.com>
2024-06-10 22:47:22 -0300

    Subindo a classe Alimento

---
+++ src/main/java/org/example/Alimento.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class Alimento extends ProdutoAbstrato{
+
+    private String dataValidade;
+
+    public Alimento(String nome, double preco, String dataValidade, int quantidade) {
+        super(nome, preco, quantidade);
+        this.dataValidade = dataValidade;
+    }
+
+    public Alimento() {}
+
+    public String getDataValidade() {
+        return dataValidade;
+    }
+
+    public void setDataValidade(String dataValidade) {
+        this.dataValidade = dataValidade;
+    }
+
+    @Override
+    public String getTipo() {
+        return "Alimento";
+    }
+
+    @Override
+    public String exibirDetalhes() {
+        return "Tipo: " + getTipo() + ", Nome: " + getNome() + ", Preço: " + getPreco() +
+                ", Quantidade: " + getQuantidade() + ", Validade: " + dataValidade;
+    }
+
+}
