af59d55a66f86dbe6ae0fe13c861cfb79a3f8dd4
Luiz Fernando <luizfernandodemoura.2013@gmail.com>
2024-06-13 10:31:37 -0300

    Testando metodos Adicionar,Remover,Listar

--- src/test/java/arrinhoDeComprasTest.java
+++ src/test/java/arrinhoDeComprasTest.java
@@ -20,7 +20,38 @@ public class arrinhoDeComprasTest {
         carrinho.adicionarItem(eletronico);
 
         assertEquals(1, carrinho.itens.size());
-        assertEquals("Smartphone", carrinho.itens.get(0).getNome());
+        assertEquals("Celular", carrinho.itens.get(0).getNome());
+    }
+
+    @Test
+    public void testRemoverItem() {
+        Eletronicos eletronico = new Eletronicos("Smartphone", 2999.0, 12, 1);
+        carrinho.adicionarItem(eletronico);
+
+        carrinho.removerItem("Celular");
+
+        assertEquals(0, carrinho.itens.size());
+    }
+
+
+    @Test
+    public void testListarItens() {
+        Eletronicos eletronico = new Eletronicos("Smartphone", 2999.0, 12, 1);
+        carrinho.adicionarItem(eletronico);
+
+        String expectedOutput = "Lista de Produtos no Carrinho:\n" +
+                "-----------------------------------------------------------\n" +
+                "Nome: Celular, Preço: 2999.0, Quantidade: 1, Garantia: 12 meses\n" +
+                "-----------------------------------------------------------\n" +
+                "Total: 2999.0\n";
+
+        // Pegar saída 
+        java.io.ByteArrayOutputStream outContent = new java.io.ByteArrayOutputStream();
+        System.setOut(new java.io.PrintStream(outContent));
+
+        carrinho.listarItens();
+
+        assertEquals(expectedOutput, outContent.toString());
     }
 
 }
